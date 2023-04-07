2cac9b8f51b22a8c3772d3346530032d59fd3bb0
Timothy Sukamtoh <timtoh168@gmail.com>
2023-04-07 16:20:07 -0700

    finish two-array-object

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,20 @@
+"use strict";
 // add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+/** Takes 2 arrays of possible different length - first is keys second is values
+ *  Return an object created from keys and vals
+ */
+function twoArrayObject(keys, values) {
+  let finalObj = {};
+
+  if(values.length < keys.length) {
+    for(let i = values.length; i < keys.length; i++) {
+      values.push(null);
+    }
+  }
+
+  for(let i = 0; i < keys.length; i++) {
+    finalObj[keys[i]] = values[i];
+  }
+
+  return finalObj;
 }
