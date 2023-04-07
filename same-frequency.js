3b010e2a9c197a10e281e60d8575d15f2dce7b70
Hannah Nguyen <hnguyen160801@gmail.com>
2023-04-07 19:59:41 -0400

    same frequency and serparate positive

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,35 @@
+"use strict";
 // add whatever parameters you deem necessary & write docstring
-function sameFrequency() {
+/**
+ *
+ * @param {*} num1
+ * @param {*} num2
+ * takes 2 numbers and compares the frequency of digits
+ * returns true if they have the same frequency of digits and false otherwise
+ */
+function sameFrequency(num1, num2) {
+  function getFrequencyCounter(string) {
+    const frequencyCounter = {};
+
+    for (let char of string) {
+      let counter = frequencyCounter[char] || 0;
+      frequencyCounter[char] = counter + 1;
+    }
+
+    return frequencyCounter;
+  }
+
+  if (num1.toString().length !== num2.toString().length) {
+    return false;
+  }
+
+  let num1Freq = getFrequencyCounter(num1.toString());
+  let num2Freq = getFrequencyCounter(num2.toString());
+
+  for (let key in num1Freq) {
+    if (num1Freq[key] !== num2Freq[key]) {
+      return false;
+    }
+  }
+  return true;
 }
--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,31 @@
+"use strict";
 // add whatever parameters you deem necessary & write docstring
-function separatePositive() {
+/**
+ * take an array of non zero numbers,
+ * separate positive numbers to left and negative to the right
+ * should return a mutated array.
+ */
+function separatePositive(nums) {
+  let right = nums.length - 1
+  let left = 0
+
+  while (left < right) {
+    console.log(nums)
+    if (nums[left] < 0) {
+      let temp = nums[left]
+      nums.splice(left, 1)
+      nums.push(temp)
+    } else {
+      left ++
+    }
+
+    if (nums[right] > 0) {
+      let temp = nums[right]
+      nums.splice(right, 1)
+      nums.unshift(temp)
+    } else {
+      right --
+    }
+  }
+  return nums;
 }
